a66de2c66b2d6656e6708a31ac763f6479d6c90e
saravanan <s.k.saravanan94@gmail.com>
2023-01-12 23:29:23 +0530

    object to action in reducer

--- src/Component/CounterTwo.js
+++ src/Component/CounterTwo.js
@@ -4,11 +4,11 @@ const initialState = {
     count : 0
 }
 const reducer = (state, action) => {
-    switch(action){
+    switch(action.type){
         case 'increment':
-            return {count : state.count + 1}
+            return {count : state.count + action.value}
         case 'decrement' :
-            return {count : state.count - 1}
+            return {count : state.count - action.value}
         case 'reset':
             return initialState
         default:
@@ -22,9 +22,9 @@ function CounterTwo() {
     return (
         <div>
             <div>Count - {newState.count}</div>
-            <button onClick={() => dispatch('increment')}>Increment</button>
-            <button onClick={() => dispatch('decrement')}>Decrement</button>
-            <button onClick={() => dispatch('reset')}>Reset</button>
+            <button onClick={() => dispatch({type:'increment', value:1})}>Increment</button>
+            <button onClick={() => dispatch({type:'decrement', value:1})}>Decrement</button>
+            <button onClick={() => dispatch({type:'reset'})}>Reset</button>
         </div>
     )
 }
