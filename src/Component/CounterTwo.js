ffee95193824469081d59ef092f292ad4443bbff
saravanan <s.k.saravanan94@gmail.com>
2023-01-12 23:36:24 +0530

    multiple variable in state

--- src/Component/CounterTwo.js
+++ src/Component/CounterTwo.js
@@ -1,16 +1,17 @@
 import React, { useReducer } from 'react'
 
 const initialState = {
-    count : 0
+    count : 0,
+    resetCount : 0
 }
 const reducer = (state, action) => {
     switch(action.type){
         case 'increment':
-            return {count : state.count + action.value}
+            return {...state, count : state.count + action.value}
         case 'decrement' :
-            return {count : state.count - action.value}
+            return {...state, count : state.count - action.value}
         case 'reset':
-            return initialState
+            return {count : 0, resetCount : state.resetCount + 1,}
         default:
             return state
     }
@@ -21,7 +22,7 @@ function CounterTwo() {
 
     return (
         <div>
-            <div>Count - {newState.count}</div>
+            <div>Count - {newState.count} ResetCount - {newState.resetCount}</div>
             <button onClick={() => dispatch({type:'increment', value:1})}>Increment</button>
             <button onClick={() => dispatch({type:'decrement', value:1})}>Decrement</button>
             <button onClick={() => dispatch({type:'reset'})}>Reset</button>
