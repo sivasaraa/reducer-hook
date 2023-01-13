ed78f51e52f121fd2cb0adb54297bbd93a24c7db
saravanan <s.k.saravanan94@gmail.com>
2023-01-13 23:32:11 +0530

    initial level reducer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import CounterOne from './Component/CounterOne';
 import CounterTwo from './Component/CounterTwo';
+import CounterThree from './Component/CounterThree';
 
 function App() {
   return (
     <div className="App">
       {/* <CounterOne /> */}
-      <CounterTwo />
+      {/* <CounterTwo /> */}
+      <CounterThree />
     </div>
   );
 }
